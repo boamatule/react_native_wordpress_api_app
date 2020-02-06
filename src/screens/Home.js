19facb6f0e42c43f0cb625474b19b2124c2d1e41
Boa <boa.matule@gmail.com>
2020-02-06 01:49:57 +0100

    Installs react-native-paper to help implement the overall of the Home Screen in the file Home.js; Uses the Card component as a parent component to wrap the sub-components. And styles the Card Component. WIP

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,14 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
+import {
+  Avatar,
+  Button,
+  Card,
+  Title,
+  Paragraph,
+  List,
+  Headline,
+} from 'react-native-paper';
 class Home extends Component {
   constructor(props) {
     super(props);
@@ -9,7 +18,30 @@ class Home extends Component {
   render() {
     return (
       <View>
-        <Text> Home </Text>
+        <Card
+          style={{
+            shadowOffset: {width: 5, height: 5},
+            width: '90%',
+            borderRadius: 12,
+            alignSelf: 'center',
+            marginBottom: 10,
+          }}>
+          <Card.Content>
+            <Title>Blog post</Title>
+            <Card.Cover
+              style={{
+                width: 350,
+                height: 190,
+                alignSelf: 'center',
+              }}
+              source={{
+                uri:
+                  'https://images.unsplash.com/photo-1573921470445-8d99c48c879f?ixlib=rb1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+              }}
+            />
+            <Paragraph>just a blog post</Paragraph>
+          </Card.Content>
+        </Card>
       </View>
     );
   }
